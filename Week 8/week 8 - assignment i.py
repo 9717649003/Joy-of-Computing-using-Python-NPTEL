4ddb0fa93f25b0c7d312752d77104dccbde3392c
9717649003 <55802269+9717649003@users.noreply.github.com>
2019-09-25 21:53:28 +0530

    Update week 8 - assignment i.py

--- Week 8/week 8 - assignment i.py
+++ Week 8/week 8 - assignment i.py
@@ -1,11 +1,15 @@
 #link for the question
 #https://onlinecourses.nptel.ac.in/noc19_cs09/progassignment?name=278
 
-s=map(int,input().split());
-d={};
+n=input()
+s=n.split()
+d=[]
 for i in s:
-  try:
-    d[i]+=1;
-  except:
-    print(i,end=' ');
-    d[i]=1;
+  d.append(int(i))
+a=[]
+for i in d:
+    if i not in a:
+        a.append(i)
+for j in range(len(a)-1):
+    print(a[j],end=" ")
+print(a[j+1],end="")
